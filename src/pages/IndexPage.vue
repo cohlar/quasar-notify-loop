61b389bf1a4d685404aa2d05f164d9166a83ed48
cohlar <cohlar@gmail.com>
2022-12-28 20:24:52 +0400

    made an example that made slightly more sense

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,29 +1,42 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <q-input type="number" v-model.number="radius" label="Circle Radius" />
-    <q-input type="number" :modelValue="area1" label="Circle Area" readonly />
-    <q-input type="number" :modelValue="area2" label="Circle Area" readonly />
-    <q-input type="number" :modelValue="area3" label="Circle Area" readonly />
-    <q-input type="number" :modelValue="area4" label="Circle Area" readonly />
-    <q-btn @click="clearRadius">Clear Radius</q-btn>
+    <q-input type="number" v-model.number="area" label="Area" />
+    <q-input
+      v-for="figure in planeFigures"
+      :key="figure"
+      type="number"
+      :modelValue="calculateFromArea[figure](area, notifyWarning)"
+      :label="label[figure]"
+      readonly
+    />
+    <q-btn @click="clearArea">Clear Area</q-btn>
   </q-page>
 </template>
 
 <script setup lang="ts">
 import { useQuasar } from 'quasar';
-import { calculateCircleArea } from 'src/stateless-logic/geometry';
-import { computed, ref } from 'vue';
+import { calculateFromArea, PlaneFigure, planeFigures } from 'src/stateless-logic/geometry';
+import { ref, watchEffect } from 'vue';
 
 const $q = useQuasar();
+const notifyWarning = (message: string) => $q.notify({ type: 'warning', message });
+const notifyError = (message: string) => $q.notify({ type: 'negative', message });
 
-const radius = ref<number | ''>(1);
+const area = ref<number | ''>(0);
 
-const area1 = computed(() => calculateCircleArea(radius.value, $q.notify));
-const area2 = computed(() => calculateCircleArea(radius.value, $q.notify));
-const area3 = computed(() => calculateCircleArea(radius.value, $q.notify));
-const area4 = computed(() => calculateCircleArea(radius.value, $q.notify));
+const clearArea = () => {
+  area.value = '';
+};
 
-const clearRadius = () => {
-  radius.value = '';
+watchEffect(() => {
+  if (area.value === '') {
+    notifyError('Please provide an area.');
+  }
+});
+
+const label: Record<PlaneFigure, string> = {
+  circle: 'Circle Radius',
+  square: 'Square Side',
+  'equilateral triangle': 'Equilateral Triangle Side',
 };
 </script>
--- src/stateless-logic/geometry.ts
+++ src/stateless-logic/geometry.ts
@@ -1,7 +1,37 @@
-export const calculateCircleArea = (radius: number | '', onError = console.error): number | '' => {
-  if (radius === '') {
-    onError('Please provide a radius to calculate the circle area');
+export const planeFigures = ['circle', 'square', 'equilateral triangle'] as const;
+export type PlaneFigure = typeof planeFigures[number];
+
+type CalculateFunction = (area: number | '', onWarning: (m: string) => unknown) => number | '';
+
+const calculateCircleRadius: CalculateFunction = (area, onWarning = console.warn) => {
+  if (area === '') return '';
+  if (area <= 1) {
+    onWarning("Your circle is too small, I won't bother calculating the radius for something so small");
     return '';
   }
-  return Math.PI * Math.pow(radius, 2);
+  return Math.pow(area / Math.PI, 0.5);
+};
+
+const calculateSquareSide: CalculateFunction = (area, onWarning = console.warn) => {
+  if (area === '') return '';
+  if (area >= 100) {
+    onWarning("Your square is too large, I don't have the energy to calculate the side for something so big");
+    return '';
+  }
+  return Math.pow(area, 0.5);
+};
+
+const calculateEqTriangleSide: CalculateFunction = (area, onWarning = console.warn) => {
+  if (area === '') return '';
+  if (area > 10 && area < 50) {
+    onWarning('Your triangle is too avergae, make it smaller or larger if you want me to calculate the side');
+    return '';
+  }
+  return Math.pow((area * 4) / Math.pow(3, 0.5), 0.5);
+};
+
+export const calculateFromArea: Record<PlaneFigure, CalculateFunction> = {
+  circle: calculateCircleRadius,
+  square: calculateSquareSide,
+  'equilateral triangle': calculateEqTriangleSide,
 };
